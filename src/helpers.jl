f1be8f235d4dd146243e2eb98e75bd964b09c2f4
Christian Dengler <christian.dengler@sms-group.com>
2022-10-28 08:41:28 +0200

    support sending payload::Number

--- src/helpers.jl
+++ src/helpers.jl
@@ -1,3 +1,5 @@
-getbytes(in::String) = transcode(UInt8, in)
-getbytes(in::AbstractVector{UInt8}) = in
-getbytes(in) = reinterpret(UInt8, in)
+# The function getbytes transforms the payload into a Vector representation of UInt8
+@inline getbytes(in::String) = transcode(UInt8, in)
+@inline getbytes(in::AbstractVector{UInt8}) = in
+@inline getbytes(in::Number) = reinterpret(UInt8, [in])
+@inline getbytes(in) = reinterpret(UInt8, in)
