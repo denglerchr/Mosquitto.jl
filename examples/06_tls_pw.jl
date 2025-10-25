46c43ecca4b9aa90fe01174424c289efe8ae5ed8
Christian Dengler <denglerchr@gmail.com>
2025-10-25 10:36:29 +0200

    fix examples 05 and 06 to not use loop_status, clean up 07

--- examples/06_tls_pw.jl
+++ examples/06_tls_pw.jl
@@ -1,26 +1,61 @@
 using Mosquitto
+using Dates
 
-# import paths to the server ca file (can be downloaded from https://test.mosquitto.org/ )
-include("authfiles/certpaths.jl")
+"""
+    main(args::AbstractVector{String})
 
-# Create client, but dont connect yet
-client = Client()
+Test for the port 8885 of test.mosquitto.org.
+The args vector must contain the file path for the certificate file for this to work.
+You can get the file on test.mosquitto.org
+"""
+function main(args::AbstractVector{String})
+    # Checking arguments
+    if length(args) < 1
+        @warn "Certificate file not provided. Giving it a shot with default filename..."
+        cafile = "mosquitto.org.crt"
+    else
+        cafile = args[1]
+    end
 
-# Configure tls using the ca certificate, needs to be done before connecting
-tls_set(client, cafile)
+    # Create client, but dont connect yet
+    client = Client()
 
-# Connect using username and password
-connect(client, "test.mosquitto.org", 8885; username = "rw", password = "readwrite")
+    # Configure tls by providing crt and key files, needs to be done before connecting
+    status = tls_set(client, cafile)
+    status != Mosquitto.MosquittoCwrapper.MOSQ_ERR_SUCCESS && error("`tls_set` returned $status")
 
-# Rest as usual, subscribe and publish and read messages
-subscribe(client, "test")
-publish(client, "test/julia", "hello"; retain = false)
-client.status.loop_status ? sleep(1) : loop(client; ntimes = 10)
+    # Connect
+    status = connect(client, "test.mosquitto.org", 8885; username = "rw", password = "readwrite")
+    status != Mosquitto.MosquittoCwrapper.MOSQ_ERR_SUCCESS && error("`connect` returned $status")
 
-nmessages = Base.n_avail(get_messages_channel(client))
-for i = 1:nmessages
-    msg = take!(get_messages_channel(client)) # Tuple{String, Vector{UInt8})
-    println("Topic: $(msg.topic)\tMessage: $(String(msg.payload))")
+    # Rest as usual, subscribe and publish and read messages
+    subscribe(client, "test/julia")
+
+    n_received = 0
+    tnext = now() + Second(1)
+    while n_received < 10
+        # network loop
+        status = loop(client)
+        if status != Mosquitto.MosquittoCwrapper.MOSQ_ERR_SUCCESS
+            error("`loop` returned $status")
+        end
+
+        # check if we have any messages
+        while !isempty(get_messages_channel(client))
+            msg = take!(get_messages_channel(client))
+            n_received += 1
+            println("Message $n_received of 10\tTopic: $(msg.topic)\tMessage: $(String(msg.payload))")
+        end
+
+        # send a message per second
+        if now() > tnext
+            tnext += Second(1)
+            publish(client, "test/julia", "Hello, so far I have received $n_received messages."; retain = false)
+        end
+    end
+
+    disconnect(client)
+    return 0
 end
 
-disconnect(client)
+main(ARGS)
